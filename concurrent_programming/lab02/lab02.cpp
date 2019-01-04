1104105733875918a667bad629fd6d5a3a3479ae
Tommac1 <tom.maczkowski@gmail.com>
2019-01-04 19:36:34 +0100

    concurrent lab02 fix

--- concurrent_programming/lab02/lab02.cpp
+++ concurrent_programming/lab02/lab02.cpp
@@ -15,7 +15,6 @@ to see the file use external application ( graphic viewer)
 #include <math.h>
 #include <vector>
 #include <thread>
-#include <mutex>
 
 const int iXmax = 800; 
 const int iYmax = 800;
@@ -34,13 +33,6 @@ double PixelHeight=(CyMax-CyMin)/iYmax;
 const int MaxColorComponentValue=255; 
 
 static unsigned char color[iYmax][iXmax][3];
-
-std::mutex color_mutex;
-
-int mask[4][3][3] = {{{0, -1, 0}, {-1, 4, -1}, {0, -1, 0}}, 
-    {{0, 1, 0}, {1, -4, 1}, {0, 1, 0}},
-    {{1, 2, 1}, {2, -8, 2}, {1, 2, 1}},
-    {{-1, 2, -1}, {2, -4, 2}, {-1, 2, -1}}};
 
 void write_line(int ymin, int ymax)
 {
@@ -99,43 +91,6 @@ void write_line(int ymin, int ymax)
 
 }
 
-void apply_mask(int x, int y, int mask[3][3])
-{
-    color_mutex.lock();
-    for (int i = y; i < iXmax / 4; ++i) {
-        for (int j = x; j < iYmax / 4; ++j) {
-            color[i][j][0] *= mask[i % 3][j % 3];
-            color[i][j][1] *= mask[i % 3][j % 3];
-            color[i][j][2] *= mask[i % 3][j % 3];
-       }
-    }
-    color_mutex.unlock();
-}
-
-void thread_filter()
-{
-    for (int i = 0; i < iXmax; i += (iXmax / 4)) {
-        for (int j = 0; j < iYmax; j += (iYmax / 4)) {
-            apply_mask(i, j, mask[i]);
-        }
-    }
-}
-
-void filter()
-{
-
-    std::vector <std::thread> vt;
-
-    for (int i = 0; i < 4; ++i) {
-        vt.push_back(std::thread(thread_filter));
-    }
-
-    for (auto &thread : vt)
-        if (thread.joinable())
-            thread.join();
-
-}
-
 int main()
 {
     /* color component ( R or G or B) is coded from 0 to 255 */
@@ -160,7 +115,6 @@ int main()
         if (thread.joinable())
             thread.join();
 
-    filter();
 
     /* write color to the file */
     fwrite(color, (800 * 800 * 3), 1, fp);
